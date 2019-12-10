d703b865158bfafde3f339be91018ce4dd9f2eb4
Abhishek, Pabbisetty (Cognizant) <Pabbisetty.Abhishek@cognizant.com>
2019-12-11 00:46:31 +0530

    hello

---
+++ micro-services/technology-service/src/test/java/com/cts/technology/service/TechnologyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.technology.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
